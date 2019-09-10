3753643dc7a7f91804e183d5e251338aabfc2d33
jeannefukumaru <fukumaruuu@gmail.com>
2019-09-10 20:42:01 +0800

    Update git-deploy.sh

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,6 +1,6 @@
 # script to automate git commits 
 # download and run chmod u+x git-deploy.sh to make executable 
-# run `bash git-deploy.sh` to stage commit then add a commit message when prompted 
+# run `git-deploy` at the command line to stage commit then add a commit message when prompted 
 # repurposed from https://www.taniarascia.com/how-to-create-and-use-bash-scripts/
 
 #!/bin/bash
